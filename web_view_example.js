c4a54fa9ef56cc5c6a044d618cc75bd1375b6508
mesode <akwe.mesode@gmail.com>
2021-05-14 15:36:06 +0100

    init

---
+++ web_view_example.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import WebView from 'react-native-webview';
+
+const WebViewExample = () => {
+    return(
+        <View style = {styles.container}>
+            <WebView 
+                source = {{ uri:
+                    "https://www.google.com/?gws_rd=cr,ssl&ei=SICcV9_EFqqk6ASA3ZaABA#q=tutorialspoint" }}
+                    />
+        </View>
+    )
+}
+export default WebViewExample;
+
+const styles = StyleSheet.create({
+    container: {
+        height: 350,
+    },
+});
